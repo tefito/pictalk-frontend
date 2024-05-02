53c0d7f6d0e418e808ad367e10d10c1e7d9daf4f
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2024-05-02 15:14:23 +0200

    added feedback popup when entering settings menu

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="container is-max-desktop" style="padding: 1rem">
+
       <body>
         <section class="section" style="padding: 0%">
           <h1 class="title is-size-3-mobile">
@@ -12,9 +13,10 @@
     </div>
   </div>
 </template>
-<script >
+<script>
 import axios from "axios";
 import account from "@/components/auth/account";
+import MiniFeedbackModal from "../../components/auth/miniFeedbackModal.vue";
 export default {
   nuxtI18n: false,
   components: {
@@ -43,6 +45,23 @@ export default {
       } catch (error) {
         console.log("error ", error);
       }
+    }
+  },
+  mounted() {
+    const user = this.$store.getters.getUser;
+    if (!user.settings.popupfeedback) {
+      setTimeout(() => {
+        this.$buefy.modal.open({
+          parent: this,
+          component: MiniFeedbackModal,
+          hasModalCard: true,
+          customClass: "custom-class custom-class-2",
+          trapFocus: true,
+          canCancel: ["escape", "x"],
+        });
+      }, 2000);
+      user.settings.popupfeedback = true;
+      this.$store.dispatch("editUser", user);
     }
   },
 };
